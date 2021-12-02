62c4a3d6b541a8426ea3015979d1f8ff07830836
Abdullah <abdullah@192.168.1.3>
2021-12-02 19:18:55 +0530

    First commit

---
+++ src/com/abdullah/ArrayListExample.java
@@ -0,0 +1,14 @@
+package com.abdullah;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        //Syntax
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println (10 / (2 + 2) + 4 * 8);
+        }
+    }
+}
